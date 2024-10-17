7c693fefc9081e53e151e5359af8cab4af725906
Shin <159926173+PoomShin@users.noreply.github.com>
2024-10-17 08:49:29 +0000

    version-2

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,5 @@
 import './Shop.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 import axios from 'axios';
 function Item(props){
     return (<div key={props.id} onClick={()=>props.callback(props)}>
@@ -7,9 +7,15 @@ function Item(props){
         id: {props.id} <br/>
         name: {props.name}<br/>
         price: {props.price}<br/>
+        <button onClick={(e)=>props.del_callback(props.id,e)}>delete</button>
+        <button onClick={(e)=>props.upd_callback(props)}>update</button>
     </div>);
 }
 export default function Shop(){
+        let id; 
+        const name_ref=useRef(null);
+        const price_ref=useRef(null);
+        const img_ref=useRef(null);
         const [products,setProducts]=useState([]);
         const URL="https://jubilant-space-eureka-976794v4v57phw95-5000.app.github.dev";
         useEffect(()=>{
@@ -26,7 +32,8 @@ export default function Shop(){
         function addCart(item){
          setCart([...cart,{id:item.id,name:item.name,price:item.price,img:item.img}]);
         }
-        const productList=products.map(item=><Item {...item} callback={addCart}/>);
+
+        const productList=products.map(item=><Item {...item} upd_callback={upProductFrom} callback={addCart} del_callback={delProduct}/>);
         const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price}
         <button onClick={()=>{
             alert('you click'+index);
@@ -38,7 +45,62 @@ export default function Shop(){
         for(let i=0;i<cart.length;i++){
             totalprice+=cart[i].price;
         }
+        function addProduct(){
+            const data={
+             name:name_ref.current.value,
+             price:price_ref.current.value,
+             img:img_ref.current.value
+            }
+            axios.post(URL+'/api/addproducts',data)
+            .then(response=>{
+                setProducts(response.data);
+            })
+            .catch(error=>{
+                console.log("error");
+            });
+        }
+
+        function delProduct(id,event){
+            event.stopPropagation();
+            axios.post(URL+'/api/delproducts/'+id)
+            .then(response=>{
+                if(response.data.status=="ok") alert("Delete product sucessfullyl");
+                setProducts(response.data.products);
+            })
+            .catch(error=>{
+                console.log("error");
+            });
+        }
+
+        function upProductFrom(item){
+            id_ref.current.value=item.id;
+            name_ref.current.value=item.name;
+            price_ref.current.value=item.price;
+            img_ref.current.value=item.img;
+            const data={
+                name:item.name,
+                price:item.price,
+                img:item.img
+            };
+            axios.post(URL+'/api/updproducts/'+id,data)
+            .then(response=>{
+                if(response.data.status=="ok") alert("Updata product sucessfullyl");
+                setProducts(response.data.products);
+            })
+            .catch(error=>{
+                console.log("error");
+            });
+        }
+        function upProduct(){
+
+        }
+
         return (<>
+        name : <input type='text' ref={name_ref}/>
+        price : <input type='text' ref={price_ref}/>
+        img : <input type='text' ref={img_ref}/>
+        <button onClick={addProduct}>add</button>
+        <button onClick={upProduct}>update</button>
         <div className='grid-container'>{productList}</div>
         <h1>Cart</h1>
         <ol>{cartList}</ol>
